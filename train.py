cf576c14af64e4814aa31e7e524c1a6d9f024266
Azam Din <azam.din@bigtincan.com>
2022-04-18 22:35:35 +0100

    update to new code

--- train.py
+++ train.py
@@ -3,11 +3,3 @@ import mlflow
 with mlflow.start_run():
     mlflow.log_param('my_param', 99)
     mlflow.log_metric('my_metric', 55555)
-    
-    mlflow.log_param('test_url', '<a href="https://www.google.com">www.google.com</a>')
-    mlflow.set_tag('test_url', '<a href="https://www.google.com">www.google.com</a>')
-    # mlflow.set_tag('mlflow.source.type','PROJECT')
-    # mlflow.set_tag(
-    #     'mlflow.source.git.repoURL', 
-    #     'https://github.com/adin786/mlflow-test.git'
-    #     )
