4560ef52c30927c77b8a98d00e5f755fcdd99d07
Azam Din <azam.din@bigtincan.com>
2022-04-18 10:04:31 +0100

    Create mlflow project

--- train.py
+++ train.py
@@ -3,3 +3,8 @@ import mlflow
 with mlflow.start_run():
     mlflow.log_param('my_param', 99)
     mlflow.log_metric('my_metric', 1.234)
+    mlflow.set_tag('mlflow.source.type','PROJECT')
+    mlflow.set_tag(
+        'mlflow.source.git.repoURL', 
+        'https://github.com/adin786/mlflow-test.git'
+        )
